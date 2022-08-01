f6bab328851303cd64c967e44fe8234d50b4691b
luanfsouza <luanferreira7777@gmail.com>
2022-08-01 02:19:25 -0300

    qqq

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,8 +4,8 @@ const UserModel = require('../models/user.module')
 routes.get("/", async (req, res) => {
     
   try {
-    const users = await UserModel.find({})
-    res.status(200).json(users);
+    // const users = await UserModel.find({})
+    res.status(200).json({message: 'oi'});
   } catch (err) {
     res.status(500).send(console.log(err));
   }
