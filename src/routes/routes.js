d0ce36fcc2849c65ac6334ef5464bf9e88b73694
luanfsouza <luanferreira7777@gmail.com>
2022-08-01 02:26:16 -0300

    ds

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,36 @@
-const routes = require('express').Router()
+const routes = require("express").Router();
+const UserModel = require("../models/user.module");
 
 routes.get("/", async (req, res) => {
-    try{
-        res.json({message: 'oi'})
-    }
-    catch(err){
-        res.status(500).send(console.log(err))
-    }
-  
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send(console.log(err));
+  }
 });
 
-module.exports = routes
+routes.post("/", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
+
+module.exports = routes;
+
+// const routes = require('express').Router()
+
+// routes.get("/", async (req, res) => {
+//     try{
+//         res.json({message: 'oi'})
+//     }
+//     catch(err){
+//         res.status(500).send(console.log(err))
+//     }
+
+// });
+
+// module.exports = routes
